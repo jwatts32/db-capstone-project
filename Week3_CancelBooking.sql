381ab7530b14e8ce45dc1e0f9f7072a8baf12dae
jwatts32 <74277904+jwatts32@users.noreply.github.com>
2023-10-14 07:43:08 -0500

    Delete Week3_CancelBooking.sql

--- Week3_CancelBooking.sql
+++
@@ -1,9 +0,0 @@
-DROP PROCEDURE IF EXISTS CancelBooking;
-DELIMITER $$ 
-CREATE PROCEDURE CancelBooking(IN booking_ID INT) 
-BEGIN
-Delete From bookings WHERE BookingID = booking_ID; 
-SELECT CONCAT("Booking ", booking_ID, " cancelled") AS "Confirmation"; 
-END$$ 
-DELIMITER ; 
-CALL CancelBooking(5);
